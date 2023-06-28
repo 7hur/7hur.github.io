29b44904b8dac4895de3d2e098f7c46bf6e57ecd
Arthur Alves <126625861+7hur@users.noreply.github.com>
2023-06-27 22:15:22 -0300

    Teste

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//inialização aos
+AOS.init({
+    duration: 2000,
+});
+
+//lightbox
+$(document).ready(function () {
+    $('.grid').isotope({
+        itemSelector: '.item',
+    });
+
+    $('.filterBtnGroup').on('click', 'li', function () {
+        var filterValue = $(this).attr('data-filter');
+        $('.grid').isotope({ filter: filterValue });
+        $('.filterBtnGroup li').removeClass('active');
+        $(this).addClass('active');
+    });
+})
